8523540f18fb7de54d6727ad25f8488f1ceea014
Rafael Aráujo <cruzarujo486@gmail .com>
2026-03-20 13:52:23 -0300

    tratamento de erros

---
+++ src/main/java/med/voll/api/controller/TratadorDeErros.java
@@ -0,0 +1,19 @@
+package med.voll.api.controller;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity tratarErroDuplicidade(DataIntegrityViolationException ex) {
+        String mensagem = "Já existe um cadastro com os dados informados (possível email ou CRM duplicado).";
+        return ResponseEntity.status(409).body(new ErroResponse(mensagem));
+    }
+
+    public record ErroResponse(String erro) {
+    }
+}
