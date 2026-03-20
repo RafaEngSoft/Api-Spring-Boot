aa5420ce425b3735a370f331c00fe2b4043396d4
Rafael Aráujo <cruzarujo486@gmail .com>
2026-03-20 18:47:04 -0300

    criação de nova tabela no banco de dados

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,14 +1,21 @@
 package med.voll.api.endereco;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record DadosEndereco(
-         String logradouro,
-         String bairro,
-         String cep,
-         String numero,
-         String uf,
-         String cidade,
-        String complemento) {
+                @NotBlank String logradouro,
+
+                @NotBlank String bairro,
+
+                @NotBlank @Pattern(regexp = "\\d{8}") String cep,
+
+                @NotBlank String numero,
+
+                @NotBlank String uf,
+
+                @NotBlank String cidade,
+
+                @NotBlank String complemento) {
 
 }
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,4 +1,5 @@
 package med.voll.api.medico;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,14 +14,15 @@ import jakarta.persistence.*;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 
-
 public class Medico {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String crm;
+    private String telefone;
 
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
@@ -29,10 +31,11 @@ public class Medico {
     private Endereco endereco;
 
     public Medico(DadosCadastroMedico dados) {
-        this.nome= dados.nome();
-        this.email= dados.email();
-        this.crm= dados.crm();
-        this.especialidade= dados.especialidade();
-        this.endereco= new Endereco(dados.endereco());
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.telefone = dados.telefone();
+        this.endereco = new Endereco(dados.endereco());
     }
 }
